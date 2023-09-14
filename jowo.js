a8c08db5869dd9c57367e3e4df9ffe29698b372d
zhrg <mr.ilolzzz@gmail.com>
2023-09-14 22:54:13 +0700

    final final b4 anim
    made some changes so its more revealy and not just skippable

--- jowo.js
+++ jowo.js
@@ -1,8 +1,21 @@
-function reveal(button, divClass) {
+function reveal(button, divClass,num) {
     button.style.display = 'none';
     var div = document.querySelector('.' + divClass);
     div.style.display = 'block';
     setTimeout(function() {
         div.style.opacity = '1';
     }, 100);
+    if (num==3) {
+        var nextbutton = document.querySelector('.revbut2');
+        nextbutton.style.display='flex';
+    } 
+    else if(num==2){
+        var nextbutton = document.querySelector('.revbut1');
+        nextbutton.style.display='flex';
+    } 
+    else if(num==1){
+        var nextbutton = document.querySelector('.closing');
+        nextbutton.style.display='block';
+    }
+
 }
